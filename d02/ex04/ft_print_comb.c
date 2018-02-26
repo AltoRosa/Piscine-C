bf7b6ba2fc2c7fc614960eba7420f4939f8fdccc
Rosana MIRALLES <rmiralle@e1r3p11.42.fr>
2018-02-26 18:23:08 +0100

    print comb :)

---
+++ d02/ex04/ft_print_comb.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmiralle <rmiralle@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/26 17:01:55 by rmiralle          #+#    #+#             */
+/*   Updated: 2018/02/26 18:22:43 by rmiralle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+void	ft_print_comb(void)
+{
+	char a;
+	char b;
+	char c;
+
+	a = 0;
+	while (a < 10)
+	{
+		b = a + 1;
+		while (b < 10)
+		{
+			c = b + 1;
+			while (c < 10)
+			{
+				ft_putchar(a + 48);
+				ft_putchar(b + 48);
+				ft_putchar(c + 48);
+				if (a != 7 || b != 8 || c != 9)
+					ft_putstr(", ");
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
+
+int		main(void)
+{
+	ft_print_comb();
+	return (0);
+}
