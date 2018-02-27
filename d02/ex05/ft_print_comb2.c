e9cc8a247d1438f547e93fd5b30a9cdc3273c282
Rosana MIRALLES <rmiralle@e2r2p15.42.fr>
2018-02-27 15:55:39 +0100

    la suiteeuh

--- d02/ex05/ft_print_comb2.c
+++ d02/ex05/ft_print_comb2.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rmiralle <rmiralle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/02/26 17:01:55 by rmiralle          #+#    #+#             */
-/*   Updated: 2018/02/27 15:43:56 by rmiralle         ###   ########.fr       */
+/*   Created: 2018/02/27 15:00:01 by rmiralle          #+#    #+#             */
+/*   Updated: 2018/02/27 15:55:21 by rmiralle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,28 +26,24 @@ void	ft_putstr(char *str)
 	}
 }
 
-void	ft_print_comb(void)
+void	ft_print_comb2(void)
 {
 	char a;
 	char b;
-	char c;
 
 	a = 0;
-	while (a < 10)
+	while (a < 100)
 	{
 		b = a + 1;
-		while (b < 10)
+		while (b < 100)
 		{
-			c = b + 1;
-			while (c < 10)
-			{
-				ft_putchar(a + 48);
-				ft_putchar(b + 48);
-				ft_putchar(c + 48);
-				if (a != 7)
-					ft_putstr(", ");
-				c++;
-			}
+			ft_putchar(a / 10 + 48);
+			ft_putchar(a % 10 + 48);
+			ft_putchar(' ');
+			ft_putchar(b / 10 + 48);
+			ft_putchar(b % 10 + 48);
+			if (a != 98 && a != 99)
+				ft_putstr(", ");
 			b++;
 		}
 		a++;
