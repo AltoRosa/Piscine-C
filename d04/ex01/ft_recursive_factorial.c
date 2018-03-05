046fb22cdb47deacddaa5cb46f7f12d826ac0452
Rosana MIRALLES <rmiralle@e3r10p12.42.fr>
2018-03-05 18:52:26 +0100

    recursive babyyy

---
+++ d04/ex01/ft_recursive_factorial.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmiralle <rmiralle@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/05 18:14:00 by rmiralle          #+#    #+#             */
+/*   Updated: 2018/03/05 18:51:50 by rmiralle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int     ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	if (nb == 0)
+		return (1);
+	return (ft_recursive_factorial(nb - 1) * nb);
+}
