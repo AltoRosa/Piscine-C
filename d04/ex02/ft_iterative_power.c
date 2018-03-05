0d984a55d0b06a405128af85ffe5bbf7f55906b1
Rosana MIRALLES <rmiralle@e3r10p14.42.fr>
2018-03-06 00:05:56 +0100

    unpeu de facctorielle et de pouvoir

--- d04/ex02/ft_iterative_power.c
+++ d04/ex02/ft_iterative_power.c
@@ -1,22 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rmiralle <rmiralle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/03/05 18:14:00 by rmiralle          #+#    #+#             */
-/*   Updated: 2018/03/06 00:04:30 by rmiralle         ###   ########.fr       */
+/*   Created: 2018/03/05 19:38:53 by rmiralle          #+#    #+#             */
+/*   Updated: 2018/03/05 19:55:26 by rmiralle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-int     ft_recursive_factorial(int nb)
+int		ft_iterative_power(int nb, int power)
 {
-	if (nb < 0)
+	int i;
+	int res;
+
+	i = 0;
+	res = 1;
+	if (power < 0)
 		return (0);
-	if (nb == 0)
-		return (1);
-	return (ft_recursive_factorial(nb - 1) * nb);
+	while (i < power)
+	{
+		res = res * nb;
+		i++;
+	}
+	return (res);
 }
--- d04/ex03/ft_recursive_power.c
+++ d04/ex03/ft_recursive_power.c
@@ -1,22 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rmiralle <rmiralle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/03/05 18:14:00 by rmiralle          #+#    #+#             */
-/*   Updated: 2018/03/06 00:04:30 by rmiralle         ###   ########.fr       */
+/*   Created: 2018/03/05 20:10:18 by rmiralle          #+#    #+#             */
+/*   Updated: 2018/03/06 00:05:22 by rmiralle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-int     ft_recursive_factorial(int nb)
+int		ft_recursive_power(int nb, int power)
 {
-	if (nb < 0)
+	if (power < 0)
 		return (0);
-	if (nb == 0)
-		return (1);
-	return (ft_recursive_factorial(nb - 1) * nb);
+	if ()
+	return(ft())
 }
